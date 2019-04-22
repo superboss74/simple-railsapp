6e5eeab6782771a0560ecdcb846ed32152163b5a
Hugo Mejia <superboss74@protonmail.com>
2019-04-22 04:14:36 +0000

    adding elements to the home page view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  # set the index page /root url
+  root 'pages#home'
+  
+  get 'pages/about'
+  get 'pages/home'
+  get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
